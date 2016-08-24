ac88e3197dee36b66e82a8feaa44c4ce697c49e7
tufedtm <tufedtm@gmail.com>
2016-08-24 09:34:47 +0300

    init

---
+++ tasks/default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import gulp from 'gulp'
+import runSequence from 'run-sequence'
+
+gulp.task('default', function () {
+  runSequence(
+    'clear',
+    ['template', 'style', 'script', 'static'],
+    ['serve', 'watch']
+  );
+});
