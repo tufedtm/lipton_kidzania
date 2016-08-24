f7d8e92e36b300b7b2dc7bc60012757ba7f932f0
tufedtm <tufedtm@gmail.com>
2016-08-24 11:34:20 +0300

    + header

--- tasks/CONST.js
+++ tasks/CONST.js
@@ -5,8 +5,8 @@ const SRC_DIR = {
   static_img: 'src/static/img/**/*',
   static_lib: 'src/static/lib/**/*',
   script_src: 'src/static/js/*.js',
-  style_src: 'src/static/style/app.styl',
-  style_watch: 'src/static/style/**/*.styl',
+  style_src: 'src/static/style/app.less',
+  style_watch: 'src/static/style/**/*.less',
   template_src: 'src/*.html',
   template_watch: 'src/**/*.html',
 };
