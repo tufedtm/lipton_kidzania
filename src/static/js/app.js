99fd65d1e0d553082295e7097f355895b2384d74
tufedtm <tufedtm@gmail.com>
2016-08-25 16:36:28 +0300

    + pop-up

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,6 +1,18 @@
 $(() => {
 
   /**
+   * mask for input type tel
+   */
+  $('input[type=tel]').mask('+7 (999) 999-99-99');
+
+
+  /**
+   * form styler
+   */
+  $('input[type=radio]').styler();
+
+
+  /**
    * faq ask add
    */
   $('[data-js-faq-ask-add-btn]').on('click', function () {
@@ -13,6 +25,32 @@ $(() => {
    */
   $('.faq__question').on('click', function () {
     $(this).parent().toggleClass('active')
+  });
+
+
+  /**
+   * pop-up
+   */
+  function showPopUp (name) {
+    $('[data-js-popup-item=' + name + ']').addClass('active');
+  }
+  function hidePopUp () {
+    $('[data-js-popup-item]').removeClass('active');
+  }
+
+  $('[data-js-popup-link]').click(function () {
+    var name = $(this).attr('data-js-popup-link');
+    showPopUp(name);
+  });
+
+  $('[data-js-popup-close]').click(function () {
+    hidePopUp();
+  });
+
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      hidePopUp();
+    }
   });
 
 });
